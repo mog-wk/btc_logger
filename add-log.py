7595fcf6df426831ccc224a5847e4ab1a9d20102
mog-wk <ms-work@mail.com>
2023-05-31 18:23:38 -0300

    improved log line for coinmarketcap.com

--- add-log.py
+++ add-log.py
@@ -1,58 +1,59 @@
 import requests
 import datetime
-import time
-from bs4 import BeautifulSoup as bs
-import os
+from bs4 import BeautifulSoup
 
 
-def get_bitcoin_data_coinmarketcap(start_date: datetime.time, retrys: int = 10) -> str:
-    # parse content
+# TODO make date variable for get btc price since date
+# TODO format date in return
+# TODO get 24hrs increment for spec..
+# TODO format spec_info better
+def get_bitcoin_data_coinmarketcap(start_date: datetime.time, trys: int = 10) -> str:
+    # parse content NOTE: url bugs out for unknow reason
     url = "https://coinmarketcap.com/currencies/bitcoin/"
-    html_resp = requests.get(url, timeout=10)
-    print(f"Parsing from: {url} \
-Status: {html_resp.status_code}\t{'OK' if html_resp.ok else 'Error'}")
+    print(f"Parsing from: {url}")
     # get price data
-    for i in range(retrys):
-        try:
-            html = bs(html_resp.text, "lxml")
-            price = html.find("div", class_="priceValue").text
-            print(price)
-            low, high = (
+    for i in range(trys):
+        for r in range(1):
+            html_resp = requests.get(url, timeout=5)
+            print(f"Status: {html_resp.status_code}\t{'OK' if html_resp.ok else 'Error'}")
+
+            html = BeautifulSoup(html_resp.text, "lxml")
+
+            info = [stat_block.find("div", class_="statsValue").text for stat_block in html.find_all("div", class_="statsBlock")]
+
+            label = ("price", "low", "high", "date", "market_cap", "diluted_market_cap", "volume")
+            data = [
+                    html.find("div", class_="priceValue").text,
                     html.find("div",class_="sc-aef7b723-0 kIYhSM").find("span",class_="sc-fe06e004-5 jXIGCe").text,
-                    html.find("div",class_="sc-aef7b723-0 gjeJMv").find("span",class_="sc-fe06e004-5 jXIGCe").text
-                    )
-            print(low, high)
-            # TODO make date variable for get btc price since date
-            # TODO format date in return
-            return "bitcoin price: {} | 24h low: {} | 24h high: {} | date: {}".format(
-                    price, low, high, datetime.datetime.now())
-        except Exception as Err:
-            print(f"{Err} retrying {i+1} / {retrys}")
-            time.sleep(4)
+                    html.find("div",class_="sc-aef7b723-0 gjeJMv").find("span",class_="sc-fe06e004-5 jXIGCe").text,
+                    datetime.datetime.now(), info[0], info[1], info[2],
+                ]
+            output_str = ""
+            for item in zip(label, data):
+                output_str += "{}: {} | ".format(item[0], item[1])
+            return output_str
 
 
-def write_to_log(line: str, log: os.path) -> bool:
-    pass
+def test() -> str:
+    url = "https://coinmarketcap.com/currencies/bitcoin/"
+    html_resp = requests.get(url, timeout=5)
+    print(f"Status: {html_resp.status_code}\t{'OK' if html_resp.ok else 'Error'}")
+
+    html = BeautifulSoup(html_resp.text, "lxml")
+
+    price = html.find("div", class_="sc-8755d3ba-0 cQuGMr coin-stats-header")
+    print(price)
+    low, high = (
+            html.find("div",class_="sc-8755d3ba-0 iausdo"),
+            html.find("div",class_="sc-8755d3ba-0 iausdo tlr"),
+            )
+    print(low, high)
+    return "bitcoin price: {} | 24h low: {} | 24h high: {} | date: {}".format(
+            price, low, high, datetime.datetime.now())
+
 
 def get_bitcoin_brl():
     pass
-
-#def get_btc_coinmarketcap(start_date: str, end_date:str) -> List[Dict]:
-    ## build url
-    #url = f"https://coinmarketcap.com/currencies/bitcoin/historical-data/?start={start_date}&end={end_date}"
-#
-    ## make request and parse tree
-    #response = requests.get(url, timeout=5)
-    #tree = lxml.html.fromstring(response.text)
-    ## extract data
-    #table = tree.find_class("cmc-table")[0]
-    #xpath_0, xpath_1 = 'div[3]/div/table/thead/tr', 'div[3]/div/table/tbody/tr/td[%d]/div'
-    ## process data
-    #cols = [_.text_content() for _ in table.xpath(xpath_0 + '/th')]
-    #dates = (_.text_content() for _ in table.xpath(xpath_1 % 1))
-    #m = map(lambda d: (float(_.text_content().replace(',', '')) for _ in table.xpath(xpath_1 % d)),
-            #range(2, 8))
-    #return [{k: v for k, v in zip(cols, _)} for _ in zip(dates, *m)]
 
 if __name__ == "__main__":
     print("Hello World")
