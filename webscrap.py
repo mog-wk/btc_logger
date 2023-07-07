77a366c32c80474676e0e8548e157067d0464cbb
mog-wk <ms-work@mail.com>
2023-07-07 17:22:04 -0300

    added monero scrapper for coinmarketcap

--- webscrap.py
+++ webscrap.py
@@ -9,8 +9,12 @@ from bs4 import BeautifulSoup
 
 # TODO make date variable for get btc price since date
 # TODO get 24hrs increment for spec..
-# TODO format spec_info better
+# TODO make high increment of data scrapping
+
 def get_bitcoin_coinmarketcap() -> tuple:
+    '''scrap bitcoin price for coin marketcap
+    returns parsed data: (price; low, high, market_cap, diluted_market_cap, volume)'''
+
     # parse content NOTE: url bugs out for unknow reason
     url = "https://coinmarketcap.com/currencies/bitcoin/"
     print(f"Parsing from: \"{url}\"")
@@ -23,12 +27,13 @@ def get_bitcoin_coinmarketcap() -> tuple:
 
     info = [stat_block.find("div", class_="statsValue").text[1:] for stat_block in html.find_all("div", class_="statsBlock")]
 
-    label = ("price", "low", "high", "market_cap", "diluted_market_cap", "volume")
+    #label = ("price", "low", "high", "market_cap", "diluted_market_cap", "volume")
+    label = ("price", "low", "high", "market_cap", "volume")
     data = [
             html.find("div", class_="priceValue").text[1:],
             html.find("div",class_="sc-aef7b723-0 kIYhSM").find("span",class_="sc-fe06e004-5 jXIGCe").text[1:],
             html.find("div",class_="sc-aef7b723-0 gjeJMv").find("span",class_="sc-fe06e004-5 jXIGCe").text[1:],
-            info[0], info[1], info[2],
+            info[0], info[2],
         ]
     output_str = ""
     output_csv = ""
@@ -38,6 +43,40 @@ def get_bitcoin_coinmarketcap() -> tuple:
     return (output_str, output_csv)
 
 
+def get_monero_coinmarketcap(DEBUG=False):
+    ''' get monero(XMR) data from coinmarketcap, all prices in USD'''
+    url = "https://coinmarketcap.com/currencies/monero/"
+    if DEBUG: print(f"Parsing from: \"{url}\"")
+    html_resp = requests.get(url, timeout=5)
+    print(f"Status: {html_resp.status_code}\t{'OK' if html_resp.ok else 'Error'}")
+    html = BeautifulSoup(html_resp.text, "lxml")
+
+    label = ("price", "low", "high", "market_cap", "volume")
+    try:
+        price = html.find("span", class_="dxubiK")
+        low, high = html.find("div", class_="sc-16891c57-0 gPFIPZ flexBetween").find_all("span")
+        market_cap, volume, density, circulating_supply, total_supply, max_supply, fully_diluted_market_cap = (i for i in html.find_all("dd", class_="sc-16891c57-0 fRWxhs base-text"))
+        data = [
+                price.text, low.text, high.text, market_cap.text.split("%")[1], volume.text.split("%")[1], '%' + density.text[:-1]
+                ]
+
+        output_str = ""
+        output_csv = ""
+        for item in zip(label, data):
+            output_str += "{}: {} | ".format(item[0], item[1])
+            output_csv += "{};".format(item[1][1:])
+        return (output_str, output_csv)
+
+    except AttributeError as e:
+        print(f"unable to recover XMR, this usually means the website is updating,\nthe scrapper should work fine in a couple minutes")
+        if DEBUG: print(f"Error: {e}")
+        raise e
+    except:
+        print(f"unable to scrap XMR unknow error prease report...")
+    return (None, None)
+
+
+    
 def test(url: str) -> str:
     # test function for convenience
     if not url:
@@ -50,11 +89,11 @@ def get_bitcoin_brl():
     pass
 
 if __name__ == "__main__":
-    line = get_bitcoin_coinmarketcap()
+    line = get_monero_coinmarketcap(DEBUG=True)
     urls = [
             "https://www.coindesk.com/price/bitcoin/",
             " www.coinbase.com/price/bitcoin ",
             "https://markets.businessinsider.com/currencies/btc-usd?op=1",
             ]
-    print(line[1])
+    print(line)
 
